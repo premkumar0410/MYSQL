1f633dc3f1e41de500af63ef067bfc90b36662e8
premkumar0410 <rprem1042004@gmail.com>
2025-09-04 12:26:56 +0530

    4-9-25 task

--- practice.sql
+++ practice.sql
@@ -12,3 +12,26 @@ alter table employee add column email varchar(30);
 insert into employee values (101,"prem",90909090909,"prem@gmail.com");
 
 select * from employee;
+
+
+use hr;
+
+delimiter $$
+create function Fullname(Firstname varchar(30) , Lastname varchar(30))
+returns varchar(60)
+deterministic
+begin	
+	declare fullname varchar(60);
+    set fullname = (concat(Firstname , " ", Lastname));
+	return fullname;
+end $$
+delimiter ;
+
+drop function fullname;
+
+
+select Fullname("prem","kumar") as full_name;
+
+
+
+
