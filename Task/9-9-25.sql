a0f00e4e049b96b12755515da71ef485b33003a9
premkumar0410 <rprem1042004@gmail.com>
2025-09-10 18:23:39 +0530

    10-9-25 task completed trigger - stored procedure

--- Task/9-9-25.sql
+++ Task/9-9-25.sql
@@ -3,10 +3,16 @@ create procedure updatedepartment(in department_name_ varchar(20),in empid_ int)
 begin 
 declare current_department_ int;
 declare current_department_id int;
+declare valid_name int;
 declare message varchar(50);
 select department_id into current_department_ from departments where department_name = department_name_;
 select department_id into current_department_id from employees where emp_id=empid_;
-if (current_department_ = current_department_id) then
+select count(*) into valid_name from departments where department_name = department_name_;
+
+if (valid_name = 0 ) then 
+	set message = 'invalid department name';
+    select message;
+elseif (current_department_ = current_department_id) then
 	set message = 'same so dont need to change';
 	select message;
 else
@@ -19,4 +25,9 @@ delimiter ;
 
 drop procedure updatedepartment;
 
-call updatedepartment('sales',102)
+call updatedepartment('saes',102);
+
+select * from employees;
+
+
+
